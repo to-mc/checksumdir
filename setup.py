60cde7f3ae64320e2cd1bc59fbe72c745073d573
Tom McCarthy <tmac.se@gmail.com>
2015-03-04 23:43:23 +0100

    metadata

--- setup.py
+++ setup.py
@@ -24,7 +24,13 @@ setup(
     classifiers=[
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         ],
 )
