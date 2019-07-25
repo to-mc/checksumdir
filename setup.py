0503cfe541196fa1b30e295ab173b1f3794c7331
Tom McCarthy <tmac.se@gmail.com>
2019-07-25 10:28:40 +0200

    Change python versions in setup.py

--- setup.py
+++ setup.py
@@ -26,11 +26,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3"
         ],
     zip_safe=False,
     package_data={"checksumdir": ["py.typed", "__init__.pyi"]}
