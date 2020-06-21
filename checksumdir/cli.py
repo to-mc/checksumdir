42ac799c9f3a0b330b9e2cf498ddce51af15c28c
Tom McCarthy <thommcc@amazon.com>
2020-06-21 20:50:54 +0200

    chore: formatting

--- checksumdir/cli.py
+++ checksumdir/cli.py
@@ -11,26 +11,44 @@ $ dirhash('/path/to/directory', 'md5')
 
 
 import argparse
+
 import checksumdir
 
 VERSION = checksumdir.__version__
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Determine the hash for directory.')
-    parser.add_argument('-v', '--version', action='version',
-                        version='checksumdir %s' % VERSION)
-    parser.add_argument('directory', help='Directory for which to generate hash.')
-    parser.add_argument('-a', '--algorithm', choices=('md5', 'sha1', 'sha256',
-                                                      'sha512'), default='md5')
-    parser.add_argument('-e', '--excluded-files', nargs='+',
-                        help='List of excluded files.')
-    parser.add_argument('-i', '--ignore-hidden', action='store_true', default=False,
-                        help='Ignore hidden files')
-    parser.add_argument('-f', '--follow-links', action='store_true', default=False,
-                        help='Follow soft links')
-    parser.add_argument('-x', '--excluded-extensions', nargs='+',
-                        help='List of excluded file extensions.')
+    parser = argparse.ArgumentParser(description="Determine the hash for directory.")
+    parser.add_argument(
+        "-v", "--version", action="version", version="checksumdir %s" % VERSION
+    )
+    parser.add_argument("directory", help="Directory for which to generate hash.")
+    parser.add_argument(
+        "-a", "--algorithm", choices=("md5", "sha1", "sha256", "sha512"), default="md5"
+    )
+    parser.add_argument(
+        "-e", "--excluded-files", nargs="+", help="List of excluded files."
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore-hidden",
+        action="store_true",
+        default=False,
+        help="Ignore hidden files",
+    )
+    parser.add_argument(
+        "-f",
+        "--follow-links",
+        action="store_true",
+        default=False,
+        help="Follow soft links",
+    )
+    parser.add_argument(
+        "-x",
+        "--excluded-extensions",
+        nargs="+",
+        help="List of excluded file extensions.",
+    )
 
     args = parser.parse_args()
     print(
@@ -40,9 +58,10 @@ def main():
             excluded_files=args.excluded_files,
             ignore_hidden=args.ignore_hidden,
             followlinks=args.follow_links,
-            excluded_extensions=args.excluded_extensions
+            excluded_extensions=args.excluded_extensions,
         )
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
